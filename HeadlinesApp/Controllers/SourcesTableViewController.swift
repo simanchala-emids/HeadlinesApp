feff9af81d457e9aed783792a1d48273c7eebee5
simanchala-emids <simanchala.pradhan@emids.com>
2018-03-05 16:53:46 +0530

    created view model "SourcesListViewModel" displaying sources

--- HeadlinesApp/Controllers/SourcesTableViewController.swift
+++ HeadlinesApp/Controllers/SourcesTableViewController.swift
@@ -12,32 +12,26 @@ import UIKit
 class SourcesTableViewController : UITableViewController {
     
     private var webservice :Webservice!
-    private var sources :[Source] = [Source]()
+    private var sourceListViewModel :SourceListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.webservice = Webservice()
-        populateSources()
-    
-    }
-    
-    private func populateSources() {
-        self.webservice.loadSources { sources in
-            self.sources = sources
+        self.sourceListViewModel = SourceListViewModel(webservice: self.webservice) {
             self.tableView.reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.sources.count
+        return self.sourceListViewModel.sourcesViewModel.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SourceTableViewCell
         
-        let source = self.sources[indexPath.row]
+        let source = self.sourceListViewModel.sourceAt(index: indexPath.row)
         cell.titleLabel.text = source.name
         cell.descriptionLabel.text = source.description
         return cell
@@ -50,9 +44,9 @@ class SourcesTableViewController : UITableViewController {
         }
         
         let indexPath = (self.tableView.indexPathForSelectedRow)!
-        let source = self.sources[indexPath.row]
+        let source = self.sourceListViewModel.sourceAt(index: indexPath.row)
         
-        headlinesTVC.source = source
+//        headlinesTVC.source = source
         
     }
    
